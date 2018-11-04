a50692ac89b04d6434fd033963fdfae573737d8f
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-11-04 19:33:41 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // use this to copy code snippets or us your browser's console
+
+class User {
+  constructor (name, interests) {
+    this.name = name
+    this.interests = interests
+  }
+
+  matchInterests (event) {
+    return event.keywords.some(word => this.interests.includes(word))
+  }
+}
